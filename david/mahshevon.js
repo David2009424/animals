6ab0b96639de9941b4eefa734da6f54ca6c12708
dhd24 <dhd241209@gmail.com>
2025-11-27 09:50:16 +0200

    qq

---
+++ david/mahshevon.js
@@ -0,0 +1,14 @@
+﻿function cheackAnswer() {
+    var num = parseInt(document.getElementById("num").value);
+    var num2 = parseInt(document.getElementById("num").value);
+    var solu = parseInt(document.getElementById("solu").value);
+
+    if (num + num2 === solu) {
+        document.getElementById("result").innerHTML = "Correct";
+    }
+    else {
+        document.getElementById("result").innerHTML = "Incorrect";
+    }
+
+    alert(num2 + num);
+}
