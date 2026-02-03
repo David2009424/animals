d7425b7447ebd5d0c0018fb8a615855ff638c05c
dhd24 <dhd241209@gmail.com>
2026-02-03 14:13:55 +0200

    finished new

---
+++ david/ex23.js
@@ -0,0 +1,19 @@
+﻿function DrawLink()
+{
+    var div1 = document.getElementById("el");
+    link = document.createElement("a");
+    link.setAttribute("href", "1.html");
+    link.setAttribute("color", "purple");
+    link.innerHTML = "Go to 1.html";
+    div1.appendChild(link);
+}
+function DrawImage()
+{
+    var div2 = document.getElementById("el");
+    image2 = document.createElement("img");
+    image2.setAttribute("src", "1.png");
+    image2.setAttribute("width", "200");
+    image2.setAttribute("height", "200");
+    div2.appendChild(image2);
+
+}
