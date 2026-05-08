f0047135adf947ac8c6d86b6d6819532a6c604ce
dhd24 <dhd241209@gmail.com>
2026-05-08 08:59:41 +0300

    dfc

--- david/ex23.js
+++ david/ex23.js
@@ -3,7 +3,7 @@
     var div1 = document.getElementById("el");
     link = document.createElement("a");
     link.setAttribute("href", "1.html");
-    link.setAttribute("color", "purple");
+    link.setAttribute("color", "red");
     link.innerHTML = "Go to 1.html";
     div1.appendChild(link);
 }
@@ -11,9 +11,32 @@ function DrawImage()
 {
     var div2 = document.getElementById("el");
     image2 = document.createElement("img");
-    image2.setAttribute("src", "1.png");
+    image2.setAttribute("src", "photos/1.jpg.jpeg");
     image2.setAttribute("width", "200");
     image2.setAttribute("height", "200");
     div2.appendChild(image2);
 
+}
+function DrawImage3(imgIndex)
+{
+    var div3 = document.getElementById("el");
+    image3 = document.createElement("img");
+    image3.setAttribute("src", "photos/" + imgIndex + ".jpg.jpeg");
+    div3.appendChild(image3);
+}
+function DrawImage4(imgHeight, imgWidth, el, imgIndex)
+{
+    var image3 = document.createElement("img");
+    image3.setAttribute("src", "photos/" + imgIndex + ".jpg.jpeg");
+    image3.setAttribute("width", imgWidth);
+    image3.setAttribute("height", imgHeight);
+    el.appendChild(image3);
+
+}
+function DrawSomeImages(num)
+{
+    var image5 = document.createElement("img");
+    image5.setAttribute("width", "200");
+    image5.setAttribute("height", "200")
+    
 }
---
+++ david/test.js
@@ -0,0 +1,61 @@
+﻿//var grades = [90, 80, 70, 60, 50];
+
+//function avg(grades) {
+//    var sum = 0;
+//    for (var i = 0; i < grades.length; i++) {
+//        sum += grades[i];
+//    }
+//    alert(sum / grades.length);
+//}
+
+//function createbutton() {
+//    var but = document.createElement("input");
+//    but.setAttribute("type", "button");
+//    but.setAttribute("value", "click me");
+//    but.setAttribute("onclick", "avg(grades)");
+//    var div = document.getElementById("123");
+//    div.appendChild(but);
+//}
+//var arr = [3,2,2,3,1]
+//function drawtable(arr) {
+//    var table1 = document.createElement("table");
+
+//    for (var i = 0; i < arr.length; i++) {
+//        var tr = document.createElement("tr");
+//        var td = document.createElement("td");
+//        tr.appendChild(td);
+//        var currentimg = document.createElement("img");
+//        currentimg.setAttribute("src", "photos/" + arr[i] + ".jpeg");
+//        td.appendChild(currentimg);
+//        table1.appendChild(tr);
+//    }
+//    var div = document.getElementById("pic");
+//    div.appendChild(table1);
+//}
+//drawtable(arr);
+
+var arr = ["jappan", "1.jpeg", "100", "china", "2.jpeg", "100"];
+function targil7(arr) {
+    var table = document.createElement("table");
+    var div = document.getElementById("tbl3");
+    div.appendChild(table);
+    var tr = document.createElement("tr");
+    var tr2 = document.createElement("tr");
+    table.appendChild(tr);
+    table.appendChild(tr2);
+    for (var i = 0; i <= arr.length; i += 3) {
+        var td1 = document.createElement("td");
+        var img = document.createElement("img");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");
+        td1.innerHTML = arr[i];
+        img.setAttribute("src", "photos/", + arr[i + 1] + ".jpeg");
+        img.setAttribute("width", arr[i + 2]);
+        td2.appendChild(img);
+        tr2.appendChild(td2);
+        tr.appendChild(td1);
+        tr.appendChild(td3);
+    }
+}
+targil7(arr);
+
